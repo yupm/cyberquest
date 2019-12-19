29dbcab6953174d8158969bdadbfa043989da085
dylanyeo22 <dylanyeo22@gmail.com>
2019-12-19 16:36:20 +0800

    Update grunt1.sh

--- stage1/scripts/grunt1.sh
+++ stage1/scripts/grunt1.sh
@@ -16,8 +16,7 @@ scene1() {
 
     create_user
     create_scene
-    cd /home/grunt1/
-    touch -- "-Jessie was here"
+    touch -- "/home/grunt1/-Jessie was here"
     echo "Hi Jessie was here, you won't get us if you don't makeitdouble"
 
     echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 1 has been setup correctly!"
