5fb231ced400918903147abf5d1e4bdd7006a587
dylanyeo22 <dylanyeo22@gmail.com>
2019-12-19 17:14:56 +0800

    Create grunt2.sh

---
+++ stage1/scripts/grunt2.sh
@@ -0,0 +1,35 @@
+create_user() {
+
+    adduser --disabled-password --gecos "" grunt2
+    echo "grunt2:JessieandJames" | sudo chpasswd
+
+}
+
+create_scene() {
+
+    cp /shared/readme2 /home/grunt2/readme
+
+
+}
+
+scene1() {
+
+    create_user
+    create_scene
+    mkdir /home/grunt2/tryHere
+    touch "/home/grunt2/.ImHere"
+    echo "JessieandJames" > "/home/grunt2/.ImHere"
+
+    echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 2 has been setup correctly!"
+
+
+}
+
+main() {
+
+scene1
+
+}
+
+main
+exit 0
