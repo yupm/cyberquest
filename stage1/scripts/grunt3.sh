209b8b00531c62630fec69e19b6e857c832be20e
dylanyeo22 <dylanyeo22@gmail.com>
2019-12-20 11:07:40 +0800

    Create grunt3.sh

---
+++ stage1/scripts/grunt3.sh
@@ -0,0 +1,33 @@
+create_user() {
+
+    adduser --disabled-password --gecos "" grunt3
+    echo "grunt3:SurrenderNow" | sudo chpasswd
+
+}
+
+create_scene() {
+
+    cp /shared/readme3 /home/grunt2/readme
+
+
+}
+
+scene1() {
+
+    create_user
+    create_scene
+    cp /shared/dataGrunt3.txt /home/grunt3/data.txt
+
+    echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 2 has been setup correctly!"
+
+
+}
+
+main() {
+
+scene1
+
+}
+
+main
+exit 0
