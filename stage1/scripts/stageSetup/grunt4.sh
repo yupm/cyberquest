b37b8ec30e069ccfbfb1c4cdc9153c3810fd8e6f
dylanyeo22 <dylanyeo22@gmail.com>
2019-12-20 14:44:48 +0800

    Update grunt4.sh

--- stage1/scripts/stageSetup/grunt4.sh
+++ stage1/scripts/stageSetup/grunt4.sh
@@ -4,12 +4,16 @@ create_user() {
 }
 
 create_scene() {
-    cp /shared/readme4 /home/grunt2/readme
+    cp /shared/readme4 /home/grunt4/readme
 }
 
 scene1() {
+    # hidden file + somewhere in file system + owned by user grunt4 + size of file
+
     create_user
     create_scene
+    mkdir /tmp/isThisIt
+    cp /shared/dataGrunt4.txt /tmp/isThisIt/.imhere
 
     echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 2 has been setup correctly!"
 }
