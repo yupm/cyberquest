f3abb9a6603f417fa6985171a3b3805eb873fd69
dylanyeo22 <dylanyeo22@gmail.com>
2019-12-23 11:11:30 +0800

    update

---
+++ stage1/scripts/stageSetup/grunt9.sh
@@ -0,0 +1,23 @@
+create_user() 
+    adduser --disabled-password --gecos "" grunt9
+    echo "grunt9:Im_Ash_Ketchup" | sudo chpasswd
+}
+
+create_scene() {
+    cp /shared/readme9 /home/grunt9readme
+}
+
+scene1() {
+    create_user
+    create_scene
+
+
+    echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 9 has been setup correctly!"
+}
+
+main() {
+    scene1
+}
+
+main
+exit 0
