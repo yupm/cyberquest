dd22759bd26161a246f7a24aae86dad8da6f806c
skyish242 <you@example.com>
2019-12-23 22:13:46 -0500

    grunt8

---
+++ stage1/scripts/stageSetup/grunt8.sh
@@ -0,0 +1,29 @@
+create_user() {
+    adduser --disabled-password --gecos "" grunt8
+    echo "grunt8:{PROTECT_WORLD_PEACE}" | sudo chpasswd
+}
+
+create_scene() {
+    cp /shared/readme8 /home/grunt8/readme
+}
+
+
+scene1() {
+    # read the file that has a dash and spacing
+
+    create_user
+    create_scene
+
+    cp /shared/dataGrunt7.jpg /home/grunt7/CatchMe.jpg
+    sudo chown grunt8:grunt8 CatchMe.jpg
+    sudo chmod 775 CatchMe.jpg
+    
+    echo "[$(date +%H:%M:%S)]: Stage 7 Scenario 1 has been setup correctly!"
+}
+
+main() {
+    scene1
+}
+
+main
+exit 0
