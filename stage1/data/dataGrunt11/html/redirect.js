d1ca9d8de891d5626c92321ccb8afa4f297de61a
dylanyeo22 <dylanyeo22@gmail.com>
2019-12-26 13:57:20 +0800

    crontab

---
+++ stage1/data/dataGrunt11/html/redirect.js
@@ -0,0 +1,14 @@
+
+var user = document.cookie.split(';');
+
+if (user == "grunt13"){
+
+    window.location = "grunt12Password.html";
+
+}
+
+else{
+
+    window.location = "nope.html";
+
+}
--- stage1/scripts/stageSetup/grunt11.sh
+++ stage1/scripts/stageSetup/grunt11.sh
@@ -1,17 +1,17 @@
 create_user() {
-    adduser --disabled-password --gecos "" grunt10
-    echo "grunt10:{TRAIL_OF_FAILURE}" | sudo chpasswd
+    adduser --disabled-password --gecos "" grunt11
+    echo "grunt11:{IM_BROCK_LEE}" | sudo chpasswd
 }
 
 create_scene() {
-    cp /shared/readme10 /home/grunt10/readme
+    cp /shared/readme11 /home/grunt11/readme
 }
 
 scene1() {
     create_user
     create_scene
-    sudo mkdir /var/www/html/grunt10/
-    sudo cp -R /shared/dataGrunt10/html/* /var/www/html/grunt10/
+    sudo mkdir /var/www/html/grunt11/
+    sudo cp -R /shared/dataGrunt11/html/* /var/www/html/grunt11/
 
     #sudo cp /shared/grunt9.conf /etc/apache2/sites-available/
     #sudo a2ensite grunt9.conf
