786caed65e2f1f6401515a9c35114acf9150fcd6
dylanyeo22 <dylanyeo22@gmail.com>
2019-12-27 16:52:22 +0800

    update

--- stage1/scripts/stageSetup/grunt12.sh
+++ stage1/scripts/stageSetup/grunt12.sh
@@ -14,16 +14,20 @@ scene1() {
     sudo cp /shared/dataGrunt12/gruntRun13.sh /var/grunt13/
     sudo cp /shared/dataGrunt12/crontab /etc/crontab
     sudo mkdir /var/scripts/grunt13
+    sudo mkdir /etc/grunt_password/
 
     adduser --disabled-password --gecos "" grunt13
     echo "grunt13:{WE_WANT_PIKACHU}" | sudo chpasswd
 
+    sudo cp /shared/pass/grunt13.txt /etc/grunt_password/grunt13.txt
+    sudo chown grunt13:grunt13 /etc/grunt_password/grunt13.txt
     sudo chown grunt13:grunt13 /var/grunt13/
     sudo chown grunt13:grunt13 /var/scripts/grunt13/
     sudo chown grunt13:grunt13 /var/grunt13/gruntRun13.sh
     sudo chmod 774 /var/grunt13/
     sudo chmod 774 /var/scripts/grunt13/
     sudo chmod 774 /var/grunt13/gruntRun13.sh
+    sudo chmod 440 /etc/grunt_password/grunt13.txt
     
     echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 12 has been setup correctly!"
 }
