7640e46d523b4025c387746d167184e247e74bad
skyish242 <you@example.com>
2019-12-27 01:31:03 -0500

    grunt8

--- stage1/scripts/stageSetup/grunt8.sh
+++ stage1/scripts/stageSetup/grunt8.sh
@@ -5,20 +5,21 @@ create_user() {
 
 create_scene() {
     cp /shared/readme8 /home/grunt8/readme
+    
+    mkdir /etc/grunt8
+    cp /shared/dataGrunt8.py /etc/grunt8/grunt8_service.py
+
+    mkdir -p /etc/grunt9/.ssh
+    cp /shared/dataGrunt9/ssh/* /etc/grunt9/.ssh
 }
 
-
 scene1() {
-    # read the file that has a dash and spacing
 
     create_user
     create_scene
 
-    cp /shared/dataGrunt7.jpg /home/grunt7/CatchMe.jpg
-    sudo chown grunt8:grunt8 CatchMe.jpg
-    sudo chmod 775 CatchMe.jpg
-    
-    echo "[$(date +%H:%M:%S)]: Stage 7 Scenario 1 has been setup correctly!"
+    sudo python3 /etc/grunt8/grunt8_service.py   
+    echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 8 has been setup correctly!"
 }
 
 main() {
--- stage1/scripts/stageSetup/grunt9.sh
+++ stage1/scripts/stageSetup/grunt9.sh
@@ -5,6 +5,8 @@ create_user() {
 
 create_scene() {
     cp /shared/readme9 /home/grunt9/readme
+    #Creates the ssh key
+    ssh-copy-id -i /etc/grunt9/.ssh/grunt9.key.pub grunt9@localhost
 }
 
 scene1() {
@@ -16,9 +18,16 @@ scene1() {
     #sudo cp /shared/grunt9.conf /etc/apache2/sites-available/
     #sudo a2ensite grunt9.conf
     #sudo service apache2 reload
-
+    
     echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 9 has been setup correctly!"
 }
+
+# scene2(){
+#     # mkdir -p /etc/grunt9/.ssh
+#     # #Create key pair grunt9.key is priv
+#     # ssh-keygen -t rsa -N "" -f /home/grunt9/.ssh/grunt9.key
+   
+# }
 
 main() {
     scene1
