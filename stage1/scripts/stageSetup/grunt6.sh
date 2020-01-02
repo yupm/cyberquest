52392e9217b8b3693b4fbb121d18e9d4f945f908
skyish242 <you@example.com>
2020-01-01 22:31:41 -0500

    readme

--- stage1/scripts/stageSetup/grunt6.sh
+++ stage1/scripts/stageSetup/grunt6.sh
@@ -5,6 +5,10 @@ create_user() {
 
 create_scene() {
     cp /shared/readme6 /home/grunt6/readme
+
+    cp /shared/dataGrunt6.txt /home/grunt6/wobbuffet.txt
+    sudo chown grunt6:grunt6 /home/grunt6/wobbuffet.txt
+    sudo chmod 775 /home/grunt6/wobbuffet.txt
 }
 
 
@@ -13,11 +17,6 @@ scene1() {
 
     create_user
     create_scene
-    # touch -- "/home/grunt1/-Jessie was here"
-    # echo "JessieandJames" > "/home/grunt1/-Jessie was here"
-    cp /shared/dataGrunt6.txt /home/grunt6/wobbuffet.txt
-    sudo chown grunt6:grunt6 /home/grunt6/wobbuffet.txt
-    sudo chmod 775 /home/grunt6/wobbuffet.txt
      
     echo "[$(date +%H:%M:%S)]: Stage 6 Scenario 1 has been setup correctly!"
 }
