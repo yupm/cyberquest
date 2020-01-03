5991b41b52a3575e35d100e439e34cb6de8898c1
dylanyeo22 <dylanyeo22@gmail.com>
2020-01-03 11:24:41 +0800

    changed challenge

---
+++ stage1/scripts/stageSetup/grunt12.sh
@@ -0,0 +1,22 @@
+create_user() {
+    adduser --disabled-password --gecos "" grunt12
+    echo "grunt12:{PIKACHU_AND_ASH}" | sudo chpasswd
+}
+
+create_scene() {
+    cp /shared/readme12 /home/grunt12/readme
+}
+
+scene1() {
+    
+    cp /shared/dataGrunt12QRCode.png /home/grunt12/code.png
+    
+    echo "[$(date +%H:%M:%S)]: Stage 1 Scenario 12 has been setup correctly!"
+}
+
+main() {
+    scene1
+}
+
+main
+exit 0
